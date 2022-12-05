61f0e741f36b922d1cf54335d0c693823b8aaeea
eecoor <eecoor@qq.com>
2022-12-05 17:30:11 +0800

    update

--- render.js
+++ render.js
@@ -7,6 +7,7 @@ let Log = require('laravel-mix/src/Log');
 class HandlebarTask extends Task {
 
     run() {
+        console.log("run::",this.data)
         this.isHbsUpdate = false;
         this.files = new FileCollection(this.data.src);
         this.precompile();
@@ -19,15 +20,17 @@ class HandlebarTask extends Task {
     }
 
     precompile() {
+        console.log("precompile::",this.data);
         const params = this.data;
 
         this.src = process.cwd() + '/' + params.src;
         this.dist = process.cwd() + '/' + params.dist;
-        this.variables = params.variables;
+        this.variables = params.options.data?params.options.data:{};
+        this._render = params.options.render;
 
         const cwd = process.cwd();
-        this.Handlebars = require('handlebars');
-        this.layouts = require('handlebars-layouts');
+        this.Handlebars = require('Handlebars');
+        this.layouts = require('Handlebars-layouts');
         this.glob = require('glob');
 
         process.chdir(this.src)
@@ -41,15 +44,35 @@ class HandlebarTask extends Task {
         this.render()
     }
 
+    mergeJson(json1, json2) {
+        var re = {};
+        for (var attr in json1) {
+          re[attr] = json1[attr];
+        }
+        for (var attr in json2) {
+          re[attr] = json2[attr];
+        }
+        return re;
+      };
+
     render() {
         const templates = this.glob.sync('**/[^_]*.hbs');
         for (const file of templates) {
             try {
-                const parsed = path.parse(file);
-                parsed.base = parsed.name + ".html";
                 const content = fs.readFileSync(file, "utf8");
                 const template = this.Handlebars.compile(content);
+                let pagevars=this.variables;
+                let outfile=file;
+                if(this._render){
+                    this._render(file,(out,vars={})=>{
+                        this.pagevars=this.mergeJson(this.pagevars,vars);
+                        outfile=out
+                    });
+                }
+                const parsed = path.parse(outfile);
+                parsed.base = parsed.name + ".html";
                 const rendered = template(this.variables);
+                
                 const outdir = this.dist + path.sep + parsed.dir;
 
                 if (!fs.existsSync(outdir)) {
@@ -73,7 +96,7 @@ class HandlebarTask extends Task {
                 const partialName =
                     parsed.dir === ""
                         ? parsed.name
-                        : parsed.dir.replace(/\.\.\/layout\//,'') + "/" + parsed.name;
+                        : (parsed.dir + "/" + parsed.name).replace(/\.\.\/layout\//,'');
 
                 if (! this.isHbsUpdate) {
                     Log.feedback(` - register partial ${file} as ${partialName}`);
